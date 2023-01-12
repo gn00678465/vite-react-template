33bd915f67f1c6e794201c716859105170c6a785
FuYu <gn02515634@gmail.com>
2023-01-12 11:28:42 +0800

    config: Integrate eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    "plugin:react-hooks/recommended",
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': ['off'],
+    'react/jsx-uses-react': ['off'],
+    'react/jsx-props-no-spreading': ['warn'],
+    'react/no-unescaped-entities': ['off'],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    'prettier/prettier': ['warn'],
+  },
+};
